d42b4530081d5fef7d6257c67e5467cb06ff5366
Luke Thomas <lukert33@gmail.com>
2015-03-31 15:53:59 -0400

    Update details on specs.

--- spec/models/echo_spec.rb
+++ spec/models/echo_spec.rb
@@ -2,12 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Echo, :type => :model do
 
-  let(:params){ {
-    "{\"message\":\"From Wikipedia, the free encyclopedia \",\"url\":\"https://en.wikipedia.org/wiki/Batman:_Arkham_City\",\"google_credentials\":\"104171572057165723377\",
-    \"chrome_token\":\"23imtculozn89v5fysbg470ep\"}"=>nil,
-     "controller"=>"echos",
-      "action"=>"create"}
-    }
+  let(:params){{
+    "{\"message\":{\"echoQuote\":\"\\\"hile a development mode is typically great for development, it’s not so grea\\\"\",\"userText\":\"sadfsdfs\"},\"url\":\"https://devcenter.heroku.com/articles/config-vars\",\"google_credentials\":\"echothatechothat@gmail.com\",\"chrome_token\":\"m4o1yhpvxcq5e0zgk9jabisun\"}"=>nil
+  }}
 
   it "should produce a hash with default values to_args class method" do
     args = Echo.to_args(params)
@@ -51,5 +48,11 @@ RSpec.describe Echo, :type => :model do
     end
     expect(user.echos.count > before).to be true
   end
+
+  it "should have user text and quoted text separately"
+
+  it "should have quotes on either end of quoted text"
+
+  it "should encoded square brackets correctly"
 
 end
